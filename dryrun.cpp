d1b02314b4a4bc805df11c9917934388fa27a6f5
Anna Kubrak <132973635+annakubrak@users.noreply.github.com>
2024-09-07 23:31:11 -0700

    Create dryrun.cpp

---
+++ dryrun.cpp
@@ -0,0 +1,42 @@
+// Specification C2 – Source File Header
+// dryRunAnnaKubrak.cpp
+// Anna Kubrak, Cisp 360
+// 1/21/24
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Specification B1 – Variables
+int integerNum = 9;
+float floatNum = 5.3;
+double doubleNum = 13;
+bool booleanVariable = true;
+char charVariable = 'a';
+string stringVariable= "hi";
+// Specification B2 – Output Variables
+// Specification C1 – Program Greeting
+int main()
+{
+//ProgramGreeting
+cout << "Dry Run, Anna Kubrak, 1/18/24" << endl;
+cout << "integerNum: " << integerNum << endl;
+cout << "floatNum: " << floatNum << endl;
+cout << "doubleNum: " << doubleNum << endl;
+cout << "booleanVariable: " << booleanVariable << endl;
+cout << "charVariable: " << charVariable << endl;
+cout << "stringVariable: " << stringVariable << endl;
+cout << "Hello World - my name is Anna.\n";
+return 0;
+}
+
+// Specification A - Commented Sample Run
+/*
+Dry Run, Anna Kubrak, 1/18/24
+integerNum: 9
+floatNum: 5.3
+doubleNum: 13
+boolean: 1
+charVar: a
+stringVar: hi
+Hello World - my name is Anna. */
